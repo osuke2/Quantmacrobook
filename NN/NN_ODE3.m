3dbd69a731fcccadeaf8d56c0e4cbc23a9518df0
平野　慧 <tantan@Scherzo.local>
2025-04-03 02:21:27 +0900

    modification

--- NN/NN_ODE3.m
+++ NN/NN_ODE3.m
@@ -4,25 +4,25 @@ clear all; close all;
 %% Problem Definition
 % ODE: dΨ/dx = e^(-2x) - 2Ψ
 % Initial condition: Ψ(0) = 0.1
-% Domain: x ∈ [0, 2]
+% Domain: x ∈ [0, 2
 
 %% Initial condition
 A = 0.1;
 
 %% Network Parameters
 N = 5;          % Number of hidden neurons
+N2 = 7;
 learning_rate = 0.01;
 max_its = 10000;
 tol = 1e-5;
-
 %% Initialize Network Parameters
 % Random initialization with Xavier/Glorot initialization
 rng(42); % For reproducibility
 v = randn(N, 1) * sqrt(2/1);  % Input weights
-v2 = randn(N, 1) * sqrt(2/1);  % Input weights
+v2 = randn(N2, N) * sqrt(2/N);  % Input weights
 theta = randn(N, 1) * sqrt(2/1);  % Hidden biases
-theta2 = randn(N, 1) * sqrt(2/1);  % Hidden biases
-w = randn(N, 1) * sqrt(2/N);  % Output weights
+theta2 = randn(N2, 1) * sqrt(2/N);  % Hidden biases
+w = randn(N2, 1) * sqrt(2/N);  % Output weights
 
 %% Generate Training Points
 nx = 50;
@@ -89,13 +89,12 @@ title('Error Analysis');
 grid on;
 
 %% Helper Functions
-function [Psi, dPsi] = forward_pass(x, v, theta, v2, theta2, w, A)
+function [Psi, dPsi] = forward_pass(x, v, v2, theta, theta2, w, A)
     N = length(v);
     n_points = length(x);
     
     x_broadcast = repmat(x, 1, N);  % [n_points × N]
     v_broadcast = repmat(v', n_points, 1);
-    v2_broadcast = repmat(v2', n_points, 1);
     theta_broadcast = repmat(theta', n_points, 1);
     theta2_broadcast = repmat(theta2', n_points, 1);
 
@@ -104,9 +103,9 @@ function [Psi, dPsi] = forward_pass(x, v, theta, v2, theta2, w, A)
     
     dydx = v_broadcast .* sigmoid_derivative(z);  
 
-    z2 = v2_broadcast .* y + theta2_broadcast; 
+    z2 = y * v2'  + theta2_broadcast; 
     y2 = sigmoid(z2);
-    dy2dx = sigmoid_derivative(z2) .* v2_broadcast .* dydx;
+    dy2dx = sigmoid_derivative(z2) .* (dydx * v2');
    
 
     Nx = y2 * w;
@@ -121,10 +120,11 @@ function [grad_v, grad_v2,grad_theta, grad_theta2, grad_w] = compute_gradients(x
     % Compute gradients using numerical differentiation
     epsilon = 1e-6;
     N = length(v);
+    N2 = length(v2);
     grad_v = zeros(size(v));
-    grad_v2 = zeros(size(v));
+    grad_v2 = zeros(size(v2));
     grad_theta = zeros(size(theta));
-    grad_theta2 = zeros(size(theta));
+    grad_theta2 = zeros(size(theta2));
     grad_w = zeros(size(w));
 
 
@@ -135,11 +135,13 @@ function [grad_v, grad_v2,grad_theta, grad_theta2, grad_w] = compute_gradients(x
         [~, dPsi_plus] = forward_pass(x, v_plus, v2, theta, theta2, w, A);
         [~, dPsi_minus] = forward_pass(x, v_minus, v2, theta, theta2, w, A);
         grad_v(i) = mean((dPsi_plus - target).^2 - (dPsi_minus - target).^2) / (2*epsilon);
-        v2_plus = v2; v2_plus(i) = v2_plus(i) + epsilon;
-        v2_minus = v; v2_minus(i) = v2_minus(i) - epsilon;
-        [~, dPsi2_plus] = forward_pass(x, v, v2_plus, theta, theta2, w, A);
-        [~, dPsi2_minus] = forward_pass(x, v, v2_minus, theta, theta2, w, A);
-        grad_v2(i) = mean((dPsi2_plus - target).^2 - (dPsi2_minus - target).^2) / (2*epsilon);
+        for j = 1:N2
+            v2_plus = v2; v2_plus(j,i) = v2_plus(j,i) + epsilon;
+            v2_minus = v2; v2_minus(j,i) = v2_minus(j,i) - epsilon;
+            [~, dPsi2_plus] = forward_pass(x, v, v2_plus, theta, theta2, w, A);
+            [~, dPsi2_minus] = forward_pass(x, v, v2_minus, theta, theta2, w, A);
+            grad_v2(i) = mean((dPsi2_plus - target).^2 - (dPsi2_minus - target).^2) / (2*epsilon);
+        end             
     end
     
     % Gradient for theta
@@ -149,12 +151,16 @@ function [grad_v, grad_v2,grad_theta, grad_theta2, grad_w] = compute_gradients(x
         [~, dPsi_plus] = forward_pass(x, v, v2, theta_plus, theta2, w, A);
         [~, dPsi_minus] = forward_pass(x, v, v2, theta_minus, theta2, w, A);
         grad_theta(i) = mean((dPsi_plus - target).^2 - (dPsi_minus - target).^2) / (2*epsilon);
-        theta2_plus = theta; theta2_plus(i) = theta2_plus(i) + epsilon;
-        theta2_minus = theta; theta2_minus(i) = theta2_minus(i) - epsilon;
-        [~, dPsi2_plus] = forward_pass(x, v, v2, theta, theta2_plus, w, A);
-        [~, dPsi2_minus] = forward_pass(x, v, v2, theta, theta2_minus, w, A);
-        grad_theta2(i) = mean((dPsi2_plus - target).^2 - (dPsi2_minus - target).^2) / (2*epsilon);
     end
+
+    % Gradient for theta2
+    for i = 1:N2
+    theta2_plus = theta2; theta2_plus(i) = theta2_plus(i) + epsilon;
+    theta2_minus = theta2; theta2_minus(i) = theta2_minus(i) - epsilon;
+    [~, dPsi2_plus] = forward_pass(x, v, v2, theta, theta2_plus, w, A);
+    [~, dPsi2_minus] = forward_pass(x, v, v2, theta, theta2_minus, w, A);
+    grad_theta2(i) = mean((dPsi2_plus - target).^2 - (dPsi2_minus - target).^2) / (2*epsilon);        
+    end     
     
     % Gradient for w
     for i = 1:N
@@ -163,7 +169,6 @@ function [grad_v, grad_v2,grad_theta, grad_theta2, grad_w] = compute_gradients(x
         [~, dPsi_plus] = forward_pass(x, v, v2, theta, theta2, w_plus, A);
         [~, dPsi_minus] = forward_pass(x, v, v2, theta, theta2, w_minus, A);
         grad_w(i) = mean((dPsi_plus - target).^2 - (dPsi_minus - target).^2) / (2*epsilon);
-        
     end
 end
 
